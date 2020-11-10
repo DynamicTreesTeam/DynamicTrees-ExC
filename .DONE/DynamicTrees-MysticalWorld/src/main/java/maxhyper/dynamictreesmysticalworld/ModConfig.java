9ed76926e6566a7408057ca2474749f1db5758c1
Harleyoc1 <harley.oconnor@icloud.com>
2020-11-10 20:52:58 +0000

    Added silkworm egg drop for Mystical World addon.

---
+++ .DONE/DynamicTrees-MysticalWorld/src/main/java/maxhyper/dynamictreesmysticalworld/ModConfig.java
@@ -0,0 +1,16 @@
+package maxhyper.dynamictreesmysticalworld;
+
+import net.minecraftforge.common.config.Config;
+
+/**
+ * @author Harley O'Connor
+ */
+@Config(modid = DynamicTreesMysticalWorld.MODID)
+public final class ModConfig {
+
+    @Config.Name("Silkworm Egg Species")
+    @Config.RequiresMcRestart()
+    @Config.Comment({"Add species resource locations to this list for them to receive the silkworm egg drop. Note that this also respects Mystical World's config so if leaf drops or the silkworm egg is disabled the drop creators won't be added."})
+    public static String[] silkwormSpecies = new String[]{"dynamictrees:oak", "dynamictrees:oakswamp", "dynamictrees:apple", "dynamictrees:spruce", "dynamictrees:megaspruce", "dynamictrees:birch", "dynamictrees:jungle", "dynamictrees:megajungle", "dynamictrees:acacia", "dynamictrees:darkoak"};
+
+}
--- .DONE/DynamicTrees-MysticalWorld/src/main/java/maxhyper/dynamictreesmysticalworld/proxy/CommonProxy.java
+++ .DONE/DynamicTrees-MysticalWorld/src/main/java/maxhyper/dynamictreesmysticalworld/proxy/CommonProxy.java
@@ -1,8 +1,19 @@
 package maxhyper.dynamictreesmysticalworld.proxy;
 
-
 import com.ferreusveritas.dynamictrees.ModConfigs;
+import com.ferreusveritas.dynamictrees.api.TreeRegistry;
+import com.ferreusveritas.dynamictrees.systems.dropcreators.DropCreator;
+import com.ferreusveritas.dynamictrees.trees.Species;
+import com.ferreusveritas.dynamictrees.trees.TreeFamilyVanilla;
 import epicsquid.mysticalworld.config.ConfigManager;
+import maxhyper.dynamictreesmysticalworld.DynamicTreesMysticalWorld;
+import maxhyper.dynamictreesmysticalworld.ModConfig;
+import maxhyper.dynamictreesmysticalworld.dropcreators.DropCreatorSilkwormEgg;
+import net.minecraft.util.ResourceLocation;
+import org.apache.logging.log4j.LogManager;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class CommonProxy {
 	
@@ -13,6 +24,28 @@ public class CommonProxy {
 		if (ModConfigs.worldGen){
 			ConfigManager.burntTrees.attempts = 0;
 		}
+
+		if (ConfigManager.silkworm.enabled && ConfigManager.silkworm.leafDrops)
+			addSilkwormDrops();
+	}
+
+	private void addSilkwormDrops () {
+		final DropCreator dropCreatorSilkwormEgg = new DropCreatorSilkwormEgg();
+
+		Arrays.asList(ModConfig.silkwormSpecies).forEach(speciesName -> {
+			try {
+				final Species species = TreeRegistry.findSpecies(new ResourceLocation(speciesName.substring(0, speciesName.indexOf(":")), speciesName.substring(speciesName.indexOf(":") + 1)));
+
+				if (!species.isValid()) {
+					LogManager.getLogger().warn("Error whilst adding silkworm egg drop creator for species '" + speciesName + "'. Could not find species with this registry name.");
+					return;
+				}
+
+				species.addDropCreator(dropCreatorSilkwormEgg);
+			} catch (Exception e) {
+				LogManager.getLogger().warn("Error whilst adding silkworm egg drop creator for species '" + speciesName + "'. Please check each entry is a valid resource location.");
+			}
+		});
 	}
 	
 	public void postInit() {
