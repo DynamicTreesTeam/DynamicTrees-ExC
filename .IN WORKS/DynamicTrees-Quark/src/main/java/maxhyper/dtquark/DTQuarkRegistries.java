31708b13149c0c7ef6d6004c2fe2214f22f8c8df
supermassimo <massimocantugarcia@gmail.com>
2021-05-02 00:22:34 -0300

    Updated 1.16.5 quark addon to latest Dynamic Trees beta

---
+++ .IN WORKS/DynamicTrees-Quark/src/main/java/maxhyper/dtquark/DTQuarkRegistries.java
@@ -0,0 +1,39 @@
+package maxhyper.dtquark;
+
+import com.ferreusveritas.dynamictrees.api.registry.TypeRegistryEvent;
+import com.ferreusveritas.dynamictrees.blocks.leaves.LeavesProperties;
+import com.ferreusveritas.dynamictrees.blocks.rootyblocks.RootyBlock;
+import com.ferreusveritas.dynamictrees.systems.DirtHelper;
+import com.ferreusveritas.dynamictrees.systems.RootyBlockHelper;
+import com.ferreusveritas.dynamictrees.trees.Species;
+import net.minecraft.block.Block;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.ForgeRegistries;
+
+@Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
+public class DTQuarkRegistries {
+
+    @SubscribeEvent
+    public static void registerLeavesPropertiesTypes (final TypeRegistryEvent<LeavesProperties> event) {
+        event.registerType(new ResourceLocation(DynamicTreesQuark.MOD_ID, "blossom"), BlossomLeavesProperties.TYPE);
+    }
+
+    @SubscribeEvent
+    public static void registerSpeciesTypes (final TypeRegistryEvent<Species> event) {
+        event.registerType(new ResourceLocation(DynamicTreesQuark.MOD_ID, "blossom"), BlossomSpecies.TYPE);
+    }
+
+    @SubscribeEvent
+    public static void onBlocksRegistry(final RegistryEvent.Register<Block> event) {
+        Block glowcelium = ForgeRegistries.BLOCKS.getValue(new ResourceLocation("quark","glowcelium"));
+        DirtHelper.registerSoil(glowcelium, DirtHelper.DIRT_LIKE);
+        DirtHelper.registerSoil(glowcelium, DirtHelper.FUNGUS_LIKE);
+
+        for (RootyBlock rooty : RootyBlockHelper.generateListForRegistry(false, DynamicTreesQuark.MOD_ID))
+            event.getRegistry().register(rooty);
+    }
+
+}
---
+++ .IN WORKS/DynamicTrees-Quark/src/main/java/maxhyper/dtquark/DynamicTreesQuark.java
@@ -0,0 +1,39 @@
+package maxhyper.dtquark;
+
+import com.ferreusveritas.dynamictrees.api.registry.RegistryHandler;
+import net.minecraft.world.biome.Biome;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import vazkii.quark.base.world.config.BiomeTypeConfig;
+import vazkii.quark.content.world.config.BlossomTreeConfig;
+import vazkii.quark.content.world.module.BlossomTreesModule;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(DynamicTreesQuark.MOD_ID)
+public class DynamicTreesQuark
+{
+    public static final String MOD_ID = "dtquark";
+
+    public DynamicTreesQuark() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::commonSetup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+
+        RegistryHandler.setup(MOD_ID);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+        for (BlossomTreeConfig config : BlossomTreesModule.trees.values()){
+            config.biomeTypes = new BiomeTypeConfig(false, Biome.Category.NONE);
+        }
+    }
+
+    private void clientSetup(final FMLClientSetupEvent event) {
+        // do something that can only be done on the client
+    }
+
+}
