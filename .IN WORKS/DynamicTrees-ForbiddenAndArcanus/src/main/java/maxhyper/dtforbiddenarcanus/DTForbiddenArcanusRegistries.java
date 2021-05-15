9b7226519db9046239d369c1aa826d46be612c75
supermassimo <massimocantugarcia@gmail.com>
2021-05-15 00:16:25 -0300

    forbidden and arcanus stuff

--- .IN WORKS/DynamicTrees-ForbiddenAndArcanus/src/main/java/maxhyper/dtforbiddenarcanus/DTForbiddenArcanusRegistries.java
+++ .IN WORKS/DynamicTrees-ForbiddenAndArcanus/src/main/java/maxhyper/dtforbiddenarcanus/DTForbiddenArcanusRegistries.java
@@ -1,14 +1,36 @@
 package maxhyper.dtforbiddenarcanus;
 
+import com.ferreusveritas.dynamictrees.DynamicTrees;
+import com.ferreusveritas.dynamictrees.api.registry.RegistryHandler;
 import com.ferreusveritas.dynamictrees.api.registry.TypeRegistryEvent;
+import com.ferreusveritas.dynamictrees.blocks.FruitBlock;
 import com.ferreusveritas.dynamictrees.blocks.leaves.LeavesProperties;
+import com.ferreusveritas.dynamictrees.init.DTConfigs;
+import com.ferreusveritas.dynamictrees.systems.dropcreators.FruitDropCreator;
+import com.ferreusveritas.dynamictrees.trees.Species;
+import com.stal111.forbidden_arcanus.init.ModItems;
 import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.ForgeRegistries;
 
 @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
 public class DTForbiddenArcanusRegistries {
+
+    public static FruitBlock GOLDEN_APPLE_FRUIT = new FruitBlock().setDroppedItem(new ItemStack(Items.GOLDEN_APPLE))
+            .setCanBoneMeal(DTConfigs.CAN_BONE_MEAL_APPLE::get);
+    public static FruitBlock CHERRY_FRUIT = new FruitBlock()
+            .setCanBoneMeal(DTConfigs.CAN_BONE_MEAL_APPLE::get);
+
+    public static void setup() {
+        RegistryHandler.addBlock(new ResourceLocation(DynamicTreesForbiddenArcanus.MOD_ID, "golden_apple_fruit"), GOLDEN_APPLE_FRUIT);
+        RegistryHandler.addBlock(new ResourceLocation(DynamicTreesForbiddenArcanus.MOD_ID, "cherry_fruit"), CHERRY_FRUIT);
+    }
 
     @SubscribeEvent
     public static void registerLeavesPropertiesTypes (final TypeRegistryEvent<LeavesProperties> event) {
@@ -18,14 +40,16 @@ public class DTForbiddenArcanusRegistries {
     @SubscribeEvent
     public static void onBlocksRegistry(final RegistryEvent.Register<Block> event) {
 
-//        for (Block slimeDirt : TinkerWorld.slimeDirt.values())
-//            DirtHelper.registerSoil(slimeDirt, SLIME_LIKE);
-//        for (EnumObject<SlimeGrassBlock.FoliageType, SlimeGrassBlock> grassTypes : TinkerWorld.slimeGrass.values())
-//            for (SlimeGrassBlock slimeGrass : grassTypes.values())
-//                DirtHelper.registerSoil(slimeGrass, SLIME_LIKE);
-//
-//        for (RootyBlock rooty : RootyBlockHelper.generateListForRegistry(true, DynamicTreesTheTwilightForest.MOD_ID))
-//            event.getRegistry().register(rooty);
+        final Species cherry = Species.REGISTRY.get(new ResourceLocation(DynamicTreesForbiddenArcanus.MOD_ID, "cherrywood"));
+
+        if (cherry.isValid()){
+            //cherry.addDropCreator(new FruitDropCreator().setFruitItem());
+            CHERRY_FRUIT.setSpecies(cherry);
+        }
+    }
+
+    @SubscribeEvent
+    public static void onItemsRegistry(final RegistryEvent.Register<Item> event) {
     }
 
 }
--- .IN WORKS/DynamicTrees-ForbiddenAndArcanus/src/main/java/maxhyper/dtforbiddenarcanus/DynamicTreesForbiddenArcanus.java
+++ .IN WORKS/DynamicTrees-ForbiddenAndArcanus/src/main/java/maxhyper/dtforbiddenarcanus/DynamicTreesForbiddenArcanus.java
@@ -8,16 +8,18 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(DynamicTreesTheTwilightForest.MOD_ID)
-public class DynamicTreesTheTwilightForest
+@Mod(DynamicTreesForbiddenArcanus.MOD_ID)
+public class DynamicTreesForbiddenArcanus
 {
     public static final String MOD_ID = "dtforbiddenarcanus";
 
-    public DynamicTreesTheTwilightForest() {
+    public DynamicTreesForbiddenArcanus() {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::commonSetup);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientSetup);
 
         MinecraftForge.EVENT_BUS.register(this);
+
+        DTForbiddenArcanusRegistries.setup();
 
         RegistryHandler.setup(MOD_ID);
     }
