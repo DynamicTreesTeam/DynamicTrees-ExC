cee47af25356e3916873d629dc298dc9de4ba04e
supermassimo <massimocantugarcia@gmail.com>
2021-05-25 01:56:58 -0300

    Added deadwood canceller and tree jocodes

--- .IN WORKS/DynamicTrees-Atum/src/main/java/maxhyper/dtatum/DTAtumRegistries.java
+++ .IN WORKS/DynamicTrees-Atum/src/main/java/maxhyper/dtatum/DTAtumRegistries.java
@@ -2,27 +2,26 @@ package maxhyper.dtatum;
 
 import com.ferreusveritas.dynamictrees.DynamicTrees;
 import com.ferreusveritas.dynamictrees.api.registry.TypeRegistryEvent;
+import com.ferreusveritas.dynamictrees.api.worldgen.FeatureCanceller;
 import com.ferreusveritas.dynamictrees.blocks.leaves.LeavesProperties;
 import com.ferreusveritas.dynamictrees.blocks.rootyblocks.RootyBlock;
 import com.ferreusveritas.dynamictrees.growthlogic.GrowthLogicKit;
-import com.ferreusveritas.dynamictrees.models.bakedmodels.BranchBlockBakedModel;
-import com.ferreusveritas.dynamictrees.models.loaders.BranchBlockModelLoader;
 import com.ferreusveritas.dynamictrees.systems.DirtHelper;
 import com.ferreusveritas.dynamictrees.systems.RootyBlockHelper;
+import com.ferreusveritas.dynamictrees.systems.dropcreators.FruitDropCreator;
 import com.ferreusveritas.dynamictrees.trees.Family;
 import com.ferreusveritas.dynamictrees.trees.Species;
+import com.ferreusveritas.dynamictrees.worldgen.cancellers.MushroomFeatureCanceller;
 import com.teammetallurgy.atum.init.AtumBlocks;
+import com.teammetallurgy.atum.init.AtumItems;
+import com.teammetallurgy.atum.world.gen.feature.DeadwoodFeature;
 import maxhyper.dtatum.growthlogic.DTAtumGrowthLogicKits;
 import maxhyper.dtatum.leavesProperties.PalmLeavesProperties;
-import maxhyper.dtatum.models.PalmLeavesBakedModel;
-import maxhyper.dtatum.models.PalmLeavesModelLoader;
-import maxhyper.dtatum.trees.PalmFamily;
 import maxhyper.dtatum.trees.PalmSpecies;
+import maxhyper.dtatum.worldgen.DeadwoodFeatureCanceller;
 import net.minecraft.block.Block;
 import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.client.event.ModelBakeEvent;
-import net.minecraftforge.client.event.ModelRegistryEvent;
-import net.minecraftforge.client.model.ModelLoaderRegistry;
+import net.minecraft.world.gen.feature.BigMushroomFeatureConfig;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -37,12 +36,19 @@ public class DTAtumRegistries {
 
     @SubscribeEvent
     public static void registerFamilyTypes (final TypeRegistryEvent<Family> event) {
-        event.registerType(new ResourceLocation(DynamicTreesAtum.MOD_ID, "palm"), PalmFamily.TYPE);
     }
 
     @SubscribeEvent
     public static void registerSpeciesTypes (final TypeRegistryEvent<Species> event) {
         event.registerType(new ResourceLocation(DynamicTreesAtum.MOD_ID, "palm"), PalmSpecies.TYPE);
+    }
+
+    /** canceller for Atum's deadwood trees. Cancells all features of type {@link DeadwoodFeature}. */
+    public static final FeatureCanceller DEADWOOD_CANCELLER = new DeadwoodFeatureCanceller<>(new ResourceLocation(DynamicTreesAtum.MOD_ID, "deadwood"));
+
+    @SubscribeEvent
+    public static void onFeatureCancellerRegistry(final com.ferreusveritas.dynamictrees.api.registry.RegistryEvent<FeatureCanceller> event) {
+        event.getRegistry().registerAll(DEADWOOD_CANCELLER);
     }
 
     @SubscribeEvent
@@ -59,6 +65,12 @@ public class DTAtumRegistries {
         DirtHelper.registerSoil(AtumBlocks.FERTILE_SOIL_TILLED, DirtHelper.DIRT_LIKE, AtumBlocks.FERTILE_SOIL);
         DirtHelper.registerSoil(AtumBlocks.LIMESTONE_GRAVEL, DirtHelper.GRAVEL_LIKE);
         DirtHelper.registerSoil(AtumBlocks.MARL, DirtHelper.MUD_LIKE);
+
+        final Species palm = Species.REGISTRY.get(new ResourceLocation(DynamicTreesAtum.MOD_ID, "palm"));
+
+        if (palm.isValid()){
+            palm.addDropCreator(new FruitDropCreator().setFruitItem(AtumItems.DATE));
+        }
 
         for (RootyBlock rooty : RootyBlockHelper.generateListForRegistry(true, DynamicTreesAtum.MOD_ID))
             event.getRegistry().register(rooty);
---
+++ .IN WORKS/DynamicTrees-Atum/src/main/java/maxhyper/dtatum/worldgen/DeadwoodFeatureCanceller.java
@@ -0,0 +1,32 @@
+package maxhyper.dtatum.worldgen;
+
+import com.ferreusveritas.dynamictrees.api.worldgen.BiomePropertySelectors;
+import com.ferreusveritas.dynamictrees.api.worldgen.FeatureCanceller;
+import com.teammetallurgy.atum.world.gen.feature.DeadwoodFeature;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.gen.feature.*;
+
+public class DeadwoodFeatureCanceller<T extends IFeatureConfig> extends FeatureCanceller {
+
+    public DeadwoodFeatureCanceller(ResourceLocation registryName) {
+        super(registryName);
+    }
+
+    //builder.addFeature(Decoration.VEGETAL_DECORATION,
+    // AtumFeatures.DEADWOOD_FEATURE
+    // .configured(NoFeatureConfig.INSTANCE)
+    // .decorated(Placements.HEIGHTMAP_SQUARE)
+    // .decorated(Placement.COUNT_EXTRA.configured(new AtSurfaceWithExtraConfig(count, extraChance, extraCount))));
+
+    @Override
+    public boolean shouldCancel(ConfiguredFeature<?, ?> configuredFeature, BiomePropertySelectors.FeatureCancellations featureCancellations) {
+        IFeatureConfig featureConfig = configuredFeature.config;
+
+        if (!(featureConfig instanceof DecoratedFeatureConfig))
+            return false;
+        featureConfig = ((DecoratedFeatureConfig) featureConfig).feature.get().config;
+
+        return featureConfig instanceof DecoratedFeatureConfig
+                && ((DecoratedFeatureConfig) featureConfig).feature.get().feature instanceof DeadwoodFeature;
+    }
+}
