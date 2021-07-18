63ae248aa82f4f1cd4703c7ac1bc1a97a3bf8e42
supermassimo <massimocantugarcia@gmail.com>
2021-07-18 19:47:09 -0300

    added patchouli manual entry

--- .DONE/DynamicTrees-Quark/src/main/java/maxhyper/dtquark/DynamicTreesQuark.java
+++ .DONE/DynamicTrees-Quark/src/main/java/maxhyper/dtquark/DynamicTreesQuark.java
@@ -1,15 +1,15 @@
 package maxhyper.dtquark;
 
+import com.ferreusveritas.dynamictrees.DynamicTrees;
 import com.ferreusveritas.dynamictrees.api.registry.RegistryHandler;
-import net.minecraft.world.biome.Biome;
-import net.minecraftforge.common.BiomeDictionary;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import vazkii.quark.base.world.config.BiomeConfig;
-import vazkii.quark.base.world.config.BiomeTypeConfig;
+import vazkii.quark.base.module.config.type.CompoundBiomeConfig;
 import vazkii.quark.content.world.config.BlossomTreeConfig;
 import vazkii.quark.content.world.module.BlossomTreesModule;
 
@@ -20,8 +20,10 @@ public class DynamicTreesQuark
     public static final String MOD_ID = "dtquark";
 
     public DynamicTreesQuark() {
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::commonSetup);
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientSetup);
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.addListener(this::commonSetup);
+        //modEventBus.addListener(this::clientSetup);
+        modEventBus.addListener(this::gatherData);
 
         MinecraftForge.EVENT_BUS.register(this);
 
@@ -30,12 +32,16 @@ public class DynamicTreesQuark
 
     private void commonSetup(final FMLCommonSetupEvent event) {
         for (BlossomTreeConfig config : BlossomTreesModule.trees.values()){
-            config.biomeConfig = BiomeConfig.fromBiomeTypes(false);
+            config.biomeConfig = CompoundBiomeConfig.fromBiomeTypes(false);
         }
     }
 
     private void clientSetup(final FMLClientSetupEvent event) {
         // do something that can only be done on the client
+    }
+
+    public void gatherData(final GatherDataEvent event) {
+        DynamicTrees.gatherTagGenerators(MOD_ID, event);
     }
 
 }
